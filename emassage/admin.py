14919ab135acf4c4f82ff0d4101d4b0dab3c7f70
cholomia <pocholomia@gmail.com>
2016-11-28 15:36:07 +0800

    admin mobile id

--- emassage/admin.py
+++ emassage/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course, Category, Lesson, Question, Choice
+from .models import Course, Category, Lesson, Question, Choice, MobileId
 
 # Register your models here.
 admin.site.register(Course)
@@ -7,3 +7,4 @@ admin.site.register(Category)
 admin.site.register(Lesson)
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(MobileId)
