2933640d25c2c8700b0f903aff6580483459b0a1
cholomia <pocholomia@gmail.com>
2016-11-28 12:50:52 +0800

    fixed import

--- emassage/urls.py
+++ emassage/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
-from emassage import views
+from .views import CourseViewSet
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'courses', views.CourseViewSet)
+router.register(r'courses', CourseViewSet)
 
 urlpatterns = [
     url(r'^api/', include(router.urls)),
--- emassage/views.py
+++ emassage/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from rest_framework import renderers, viewsets
-from .serializers import CourseSerializer
+from rest_framework import viewsets
+
 from .models import Course
+from .serializers import CourseSerializer
 
 
 # Create your views here.
