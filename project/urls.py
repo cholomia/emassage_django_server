8175869eb75f676eb720149517b82f2675aeae19
cholomia <pocholomia@gmail.com>
2016-11-28 12:58:07 +0800

    fiexd url

--- project/urls.py
+++ project/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^emassage/', include('emassage.urls'))
+    url(r'^emassage/$', include('emassage.urls'))
 ]
 
 if settings.DEBUG:
