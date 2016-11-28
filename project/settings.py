3c4ae3b90b1687200989893e529a35a7c338d137
cholomia <pocholomia@gmail.com>
2016-11-28 14:09:28 +0800

    change mdia root

--- project/settings.py
+++ project/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.environ.get('OPENSHIFT_DATA_DIR', '')
 MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
